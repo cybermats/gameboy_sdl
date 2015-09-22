c3c163f31e12b5695e933484bfa6a229c7209eb7
= <cybermats@gmail.com>
2015-09-22 23:37:06 +0200

    Changing controls from WASD to arrows.

--- gbu_sdl_wrapper.h
+++ gbu_sdl_wrapper.h
@@ -36,28 +36,36 @@ private:
     virtual void KeyDown(SDL_Keysym keysym) override {
 		switch (keysym.sym)
 		{
-		case SDLK_w:
+		case SDLK_UP:
+			std::cout << "KeyDown Up" << std::endl;
 			_gbu->joypad()->KeyDown(Joypad::Up);
 			break;
-		case SDLK_s:
+		case SDLK_DOWN:
+			std::cout << "KeyDown Down" << std::endl;
 			_gbu->joypad()->KeyDown(Joypad::Down);
 			break;
-		case SDLK_a:
+		case SDLK_LEFT:
+			std::cout << "KeyDown Left" << std::endl;
 			_gbu->joypad()->KeyDown(Joypad::Left);
 			break;
-		case SDLK_d:
+		case SDLK_RIGHT:
+			std::cout << "KeyDown Right" << std::endl;
 			_gbu->joypad()->KeyDown(Joypad::Right);
 			break;
 		case SDLK_o:
+			std::cout << "KeyDown A" << std::endl;
 			_gbu->joypad()->KeyDown(Joypad::A);
 			break;
 		case SDLK_p:
+			std::cout << "KeyDown B" << std::endl;
 			_gbu->joypad()->KeyDown(Joypad::B);
 			break;
 		case SDLK_RETURN:
+			std::cout << "KeyDown Start" << std::endl;
 			_gbu->joypad()->KeyDown(Joypad::Start);
 			break;
 		case SDLK_SPACE:
+			std::cout << "KeyDown Select" << std::endl;
 			_gbu->joypad()->KeyDown(Joypad::Select);
 			break;
 		default:
@@ -68,28 +76,36 @@ private:
     virtual void KeyUp(SDL_Keysym keysym) override {
 		switch (keysym.sym)
 		{
-		case SDLK_w:
+		case SDLK_UP:
+			std::cout << "KeyUp Up" << std::endl;
 			_gbu->joypad()->KeyUp(Joypad::Up);
 			break;
-		case SDLK_s:
+		case SDLK_DOWN:
+			std::cout << "KeyUp Down" << std::endl;
 			_gbu->joypad()->KeyUp(Joypad::Down);
 			break;
-		case SDLK_a:
+		case SDLK_LEFT:
+			std::cout << "KeyUp Left" << std::endl;
 			_gbu->joypad()->KeyUp(Joypad::Left);
 			break;
-		case SDLK_d:
+		case SDLK_RIGHT:
+			std::cout << "KeyUp Right" << std::endl;
 			_gbu->joypad()->KeyUp(Joypad::Right);
 			break;
 		case SDLK_o:
+			std::cout << "KeyUp A" << std::endl;
 			_gbu->joypad()->KeyUp(Joypad::A);
 			break;
 		case SDLK_p:
+			std::cout << "KeyUp B" << std::endl;
 			_gbu->joypad()->KeyUp(Joypad::B);
 			break;
 		case SDLK_RETURN:
+			std::cout << "KeyUp Start" << std::endl;
 			_gbu->joypad()->KeyUp(Joypad::Start);
 			break;
 		case SDLK_SPACE:
+			std::cout << "KeyUp Select" << std::endl;
 			_gbu->joypad()->KeyUp(Joypad::Select);
 			break;
 		default:
